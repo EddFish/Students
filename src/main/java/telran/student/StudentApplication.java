77a1c129488aab1f940d04a95f568756f5fc756f
edward <edward@EdwardLFish>
2017-12-27 01:08:54 +0200

    Commit for Heroku

--- src/main/java/telran/student/StudentApplication.java
+++ src/main/java/telran/student/StudentApplication.java
@@ -3,6 +3,7 @@ package telran.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,36 +27,43 @@ public class StudentApplication {
 	}
 	
 	@PostMapping(StudentsConstants.STUDENT)
+	@CrossOrigin
 	public boolean addStudent(@RequestBody Student student) {
 		return students.addStudent(student);
 	}
 	
 	@DeleteMapping(StudentsConstants.STUDENT)
+	@CrossOrigin
 	public boolean removeStudent(@RequestParam String email) {
 		return students.removeStudent(email);
 	}
 	
 	@PutMapping(StudentsConstants.STUDENT)
+	@CrossOrigin
 	public boolean updateStudent(@RequestBody Student student) {
 		return students.editStudent(student);
 	}
 	
 	@GetMapping(StudentsConstants.STUDENT)
+	@CrossOrigin
 	public Student getStudent(@RequestParam String email) {
 		return students.getStudentById(email);
 	}
 	
 	@GetMapping(StudentsConstants.STUDENTS)
+	@CrossOrigin
 	public Iterable<Student> getStudents() {
 		return students.getAllStudents();
 	}
 	
 	@GetMapping(StudentsConstants.NAME)
+	@CrossOrigin
 	public Iterable<Student> getStudents(@RequestParam String name) {
 		return students.getStudentsByName(name);
 	}
 	
 	@GetMapping(StudentsConstants.ROOM)
+	@CrossOrigin
 	public Iterable<Student> getStudents(@RequestParam int min, @RequestParam int max) {
 		return students.getStudentsByRoom(min, max);
 	}
